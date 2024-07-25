69014416e3eb1e63eccbef84cae4bebb5942f3f0
TakumiWada <t.wada@fujinetworks.co.jp>
2024-07-25 16:31:18 +0900

    Render用セットアップ

--- server/index.js
+++ server/index.js
@@ -1,16 +1,19 @@
 
 const express = require('express')
 const mongoose = require('mongoose');
-const config = require('./config/dev')
+const config = require('./config')
 const FakeDb = require('./fake-db')
 
 const productRoutes = require('./routes/products')
+const path = require('path')
 
   mongoose.connect(config.DB_URI)
   .then(
     () => {
-    const fakeDb = new FakeDb();
-    fakeDb.initDb();
+      if(process.env.NODE_ENV !== 'production'){
+        const fakeDb = new FakeDb();
+        // fakeDb.initDb();
+      }
   }
 );
 
@@ -19,14 +22,24 @@ const app = express()
 
 app.use('/api/v1/products',productRoutes)
 
+if(process.env.NODE_ENV === 'production'){
+  const appPath = path.join(__dirname, '..', 'dist','reservation-app')
+  app.use(express.static(appPath))
+  app.get("*", function(req, res){
+    res.sendFile(path.resolve(appPath, 'index.html'))
+  })
+}
+
+
+
 const PORT = process.env.PORT || '3001'
 
 app.listen(PORT, function(){
     console.log('I am runnning')
 })
 
-const v8 = require('v8');
+// const v8 = require('v8');
 
-const maxHeapSize = 256; // MB単位で指定
-v8.setFlagsFromString(`--max-old-space-size=${maxHeapSize}`);
+// const maxHeapSize = 256; // MB単位で指定
+// v8.setFlagsFromString(`--max-old-space-size=${maxHeapSize}`);
 
